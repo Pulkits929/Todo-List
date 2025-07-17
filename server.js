61d9dc40f41da85d2209a7d3c0652677b82c74be
Pulkit Sharma <spulkit929@gmail.com>
2025-07-17 13:41:55 +0530

    changed mongoose.connect statement

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 const mongoose = require("mongoose");
-require("dotenv").config();
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB Atlas!");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
 });
   
 const trySchema = new mongoose.Schema({
